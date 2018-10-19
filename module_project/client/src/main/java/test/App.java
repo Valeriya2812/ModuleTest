97aaa241741a44217148965b032dc9fc044b3f32
Valeriia Chernikova <vchernik@softserveinc.com>
2018-10-19 17:54:15 +0300

    First commit

---
+++ module_project/client/src/main/java/test/App.java
@@ -0,0 +1,11 @@
+package test;
+import ua.module_project.servertest.MyServer;
+
+public class App {
+    public static void main(String[] args) {
+        MyServer ob=new MyServer();
+        System.out.println("Client");
+        System.out.println(ob.toString());
+
+    }
+}
---
+++ module_project/server/src/main/java/servertest/MyServer.java
@@ -0,0 +1,9 @@
+package ua.module_project.servertest;
+
+public class MyServer {
+
+    @Override
+    public String toString() {
+        return "My server";
+    }
+}
